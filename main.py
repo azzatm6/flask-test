3a7f4a5cfea3071d829f392ea57cda621476c404
azzatm6 <102761363+azzatm6@users.noreply.github.com>
2023-03-02 00:15:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask
+from flask import request
 
+#  , render_template
 app = Flask(__name__)
 
+app.route("/")
+def homepage():
+    return "hello"
+    # return render_template("Pipfile.py") 
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+@app.route("/a")
+def test():
+    return "hello"
 
+@app.post("/xvalue")
+def value():
 
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    reqXvalue = request.form['name1'] or 0
+    reqYvalue = request.form['name2'] or 0
+    reqXYvalue =  reqXvalue +" "+  reqYvalue
+    return "Hello "+ reqXYvalue +" in our project"
+    
+    
+
+# @app.post("/yvalue")
+# def yValue():
+#    reqYvalue = request.form['name2'] or 0
+#    return {"value": reqYvalue}
+
+# app.route("/from Flask")
+# def about():
+#     return "from Flask"
+
+if __name__ == "__main__":
+    app.run()
+    # debug=True, port =9000
+    # from flask import Flask
+    # Flask(app, host="0.0.0.0", port=8080)
